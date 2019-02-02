bc2dfdf96c76978fdcab45237dfee84a587fabbe
Lucas Pinheiro <lucas_p@outlook.com>
2019-02-02 21:29:33 -0200

    More tests

--- tests/Event.lua
+++ tests/Event.lua
@@ -1,10 +1,6 @@
 --TODO criar um git de testes genéricos
 --Faz algo similar ao busted (it, mocks, asserts)
 
---[[
-    Number in the event
-]]
-
 local test = {}
 local function runTest(testName)
     if not testName then
@@ -22,6 +18,7 @@ local function runTest(testName)
             print("SUCCESS\t\t" .. testName)
         else
             print("FAIL\t\t" .. testName)
+            print("\t" .. message)
         end
     else
         error("Attempt to run unexistent test '" .. testName .. "'")
@@ -39,7 +36,7 @@ function test.simpleEventListener()
     Event.listen({"SimpleEvent"}, listenerFunction)
     Event.broadcast("SimpleEvent")
 
-    assert(functionWasCalled)
+    assert(functionWasCalled, "Listener was not called.")
 end
 
 --register a listener for a simple event, deregister the listener, broadcast the event, function is not called
@@ -52,7 +49,7 @@ function test.unlistenEvent()
     Event.unlisten({"SimpleEvent"}, listenerFunction)
     Event.broadcast("SimpleEvent")
 
-    assert(not functionWasCalled)
+    assert(not functionWasCalled, "Listener was called.")
 end
 
 --register a listener for a simple event two times, broadcast the event, function is called twice
@@ -65,7 +62,7 @@ function test.listenTwiceEvent()
     Event.listen({"SimpleEvent"}, listenerFunction)
     Event.broadcast("SimpleEvent")
 
-    assert(functionWasCalled==2)
+    assert(functionWasCalled==2, "Listener was not called twice.")
 end
 
 --register a listener for a simple event two times, deregister the listener, broadcast the event, function is not called
@@ -79,7 +76,7 @@ function test.unlistenTwiceEvent()
     Event.unlisten({"SimpleEvent"}, listenerFunction)
     Event.broadcast("SimpleEvent")
 
-    assert(not functionWasCalled)
+    assert(not functionWasCalled, "Listener was called.")
 end
 
 --register a listener for first parameter of a composite event, broadcast the event, function is called
@@ -91,7 +88,7 @@ function test.compositeEventListener()
     Event.listen({"CompositeEvent"}, listenerFunction)
     Event.broadcast("CompositeEvent", "SubEvent")
 
-    assert(functionWasCalled)
+    assert(functionWasCalled, "Listener was not called.")
 end
 
 --register a listener for two parameters of a composite event, broadcast the event, function is called
@@ -103,7 +100,7 @@ function test.compositeTwoEventListener()
     Event.listen({"CompositeEvent", "SubEvent"}, listenerFunction)
     Event.broadcast("CompositeEvent", "SubEvent")
 
-    assert(functionWasCalled)
+    assert(functionWasCalled, "Listener was not called.")
 end
 
 --register a listener for two parameters of a composite event, broadcast the event with wrong second parameter,
@@ -116,7 +113,7 @@ function test.compositeWrongEventListener()
     Event.listen({"CompositeEvent", "SubEvent"}, listenerFunction)
     Event.broadcast("CompositeEvent", "AnotherSubEvent")
 
-    assert(not functionWasCalled)
+    assert(not functionWasCalled, "Listener was called.")
 end
 
 --register a listener for a non-table event, should error
@@ -135,7 +132,7 @@ function test.listenerWithoutFunction()
     local success = pcall(function()
         Event.listen({"SimpleEvent"}, notAFunction)
     end)
-    assert(not success)
+    assert(not success, "Did not error.")
 end
 
 --register a listener for a composite event, broadcast the event with the first parameter only,
@@ -148,7 +145,7 @@ function test.missedListener()
     Event.listen({"CompositeEvent", "SubEvent"}, listenerFunction)
     Event.broadcast("CompositeEvent")
 
-    assert(not functionWasCalled)
+    assert(not functionWasCalled, "Listener was called.")
 end
 
 --register a listener for a composite event with the number 1, broadcast the event with the first parameter only,
@@ -161,7 +158,35 @@ function test.eventWithNumber()
     Event.listen({"Event", 1}, listenerFunction)
     Event.broadcast("Event")
 
-    assert(not functionWasCalled)
+    assert(not functionWasCalled, "Listener was called.")
+end
+
+--register a listener for the empty event, broadcast any event, function should be called
+function test.emptyEventListener()
+    local functionWasCalled = 0
+    local function listenerFunction()
+        functionWasCalled = functionWasCalled + 1
+    end
+    Event.listen({}, listenerFunction)
+    Event.broadcast("Event1")
+    Event.broadcast("Event2", "SubEvent1")
+    Event.broadcast("Event3", "SubEvent2", "SubEvent3")
+
+    assert(functionWasCalled==3, "Listener was not called thrice.")
+end
+
+--listener receives events as parameters
+function test.eventParameters()
+    local param1, param2, param3
+    local function listenerFunction(...)
+        param1, param2, param3 = ...
+    end
+    Event.listen({"CompositeEvent"}, listenerFunction)
+    Event.broadcast("CompositeEvent", "SubEvent1", "SubEvent2")
+
+    assert(param1=="CompositeEvent", "First parameter was not 'CompositeEvent'.")
+    assert(param2=="SubEvent1", "Second parameter was not 'SubEvent1'.")
+    assert(param3=="SubEvent2", "Third parameter was not 'SubEvent2'.")
 end
 
 runTest(...)

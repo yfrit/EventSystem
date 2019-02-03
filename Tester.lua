0da303e0d84875bc758a79ae8290c9a491ce7128
Lucas Pinheiro <lucas_p@outlook.com>
2019-02-03 01:13:25 -0200

    Magic Mocks

--- Tester.lua
+++ Tester.lua
@@ -1,20 +1,48 @@
 --TODO criar um git de testes genéricos
 --Faz algo similar ao busted (it, mocks, asserts)
+--Deixar mais eficiente (O(n²) é ridículo)
+    --Isso vai bugar quanto tiver um arquivo mockando outro que está sendo testado também
 
-local test = require("tests.Event")
+function _G.magicMock()
+    local mock = setmetatable({}, {
+        __index = function(t, k)
+            local newMock = magicMock()
+            rawset(t, k, newMock)
+            return newMock
+        end,
+        __call = function()
+            return magicMock()
+        end
+    })
+    return mock
+end
 
-local function runTest(testName)
-    if not testName then
+function _G.mockRequire(path)
+    local mock = magicMock()
+    package.loaded[path] = mock
+    return mock
+end
+
+local tests = {
+    -- Event = require("tests.Event"),
+    Eventer = require("tests.Eventer"),
+}
+
+local function runTest(fileName, testName)
+    if not fileName then
         --running from console
-        for name in pairs(test) do
-            os.execute("lua .\\Tester.lua " .. name)
+        for fileName,testFile in pairs(tests) do
+            for testName in pairs(testFile) do
+                os.execute("lua .\\Tester.lua " .. fileName .. " " .. testName)
+            end
         end
         return
     end
 
     --running from execute
-    if test[testName] then
-        local success, message = pcall(test[testName])
+    local test = tests[fileName][testName]
+    if test then
+        local success, message = pcall(test)
         if success then
             print("SUCCESS\t\t" .. testName)
         else

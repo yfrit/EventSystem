efef5db7ab5e14820625ab77145037f68245fc9b
Lucas Pinheiro <lucas_p@outlook.com>
2019-02-03 01:12:58 -0200

    Tests for Eventer

---
+++ Eventer.lua
@@ -0,0 +1,29 @@
+--[[
+    TODO
+
+    Unlisten
+    Nested listeners
+--]]
+local Class = require("Class.Class")
+local Event = require("Event")
+
+local Eventer = Class.new({
+    --static things
+
+},
+function(self)
+    
+end)
+
+function Eventer:broadcast(...)
+    Event.broadcast(...)
+end
+
+function Eventer:listen(event, method, ...)
+    local function methodWithSelf(...)
+        method(self, ...)
+    end
+    Event.listen(event, methodWithSelf, ...)
+end
+
+return Eventer
